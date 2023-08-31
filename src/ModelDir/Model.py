75a9bff169a955f17809c24fcfc5c4a615fe5b6e
Екатерина Алимаскина <72378464+alimaskina@users.noreply.github.com>
2023-08-31 21:10:54 +0300

    Update Model.py

--- src/ModelDir/Model.py
+++ src/ModelDir/Model.py
@@ -1,3 +1,4 @@
+import requests
 from src.ConfigDir.Config import Config
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
@@ -8,33 +9,13 @@ class Model:
 
     def __init__(self):
         self.__config = Config()
-        self.tokenizer = AutoTokenizer.from_pretrained('../my_tg_token_1')
-        self.model = AutoModelForCausalLM.from_pretrained('../my_tg_model_1')
+        self.url = self.__config.get_url()
 
     def answer(self, context):
-        # Форматируем запрос
-        context = [' '] * (3 - len(context)) + context
-        text = f'@@ПЕРВЫЙ@@ {context[0]} @@ВТОРОЙ@@ {context[1]} @@ПЕРВЫЙ@@ {context[2]} @@ВТОРОЙ@@ '
-
         # Делаем запрос к модели
-        inputs = self.tokenizer(text, return_tensors='pt')
-        generated_token_ids = self.model.generate(
-            **inputs,
-            top_k=10,
-            top_p=0.95,
-            num_beams=3,
-            num_return_sequences=3,
-            do_sample=True,
-            no_repeat_ngram_size=2,
-            temperature=1.2,
-            repetition_penalty=1.2,
-            length_penalty=1.0,
-            eos_token_id=50257,
-            max_new_tokens=40
-        )
-
-        # Декодируем response и вытаскиваем сам ответ на сообщение
-        context_with_response = [self.tokenizer.decode(sample_token_ids) for sample_token_ids in generated_token_ids]
-        response = context_with_response[0].split("@@")[8]
-
-        return response
+        data = {'context': context}
+        response = requests.post(self.url, json=data)
+        answer = response.content.decode("utf-8")
+        
+        return answer
+    
